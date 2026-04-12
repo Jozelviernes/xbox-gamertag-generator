8fe5eb7ec07c6d9fc7f37cd1216df93c973c9e9f
Jozelviernes <viernesjozel6@gmail.com>
2026-04-12 18:23:29 +0800

    temp setup route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GamertagController;
 use App\Http\Controllers\PriceCheckerController;
-
+use Illuminate\Support\Facades\Artisan;
 
 Route::post('/generate-gamertags', [GamertagController::class, 'generate']);
 Route::post('/check-price', [PriceCheckerController::class, 'check']);
+
+Route::get('/run-setup', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    $migrateOutput = Artisan::output();
+
+    Artisan::call('db:seed', ['--class' => 'FullGeneratorSeeder']);
+    $seeder1Output = Artisan::output();
+
+    Artisan::call('db:seed', ['--class' => 'GamertagValueSeeder']);
+    $seeder2Output = Artisan::output();
+
+    return response()->json([
+        'migrate' => $migrateOutput,
+        'seeder1' => $seeder1Output,
+        'seeder2' => $seeder2Output,
+        'done'    => true,
+    ]);
+});
